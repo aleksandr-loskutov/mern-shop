1654acc6a4bcaa694244e6a992ffa34b1738bdbe
Aleksandr.L <aleksandranger@gmail.com>
2021-09-23 22:07:32 +0300

    user Schema

---
+++ server/models/models.js
@@ -0,0 +1,6 @@
+const { Schema, model } = require("mongoose");
+const User = new Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true, default: "user" }
+});
