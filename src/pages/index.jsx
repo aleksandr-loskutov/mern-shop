9c33f4fdca0658bc4fed61328d8eaf54b545e9b2
Aleksandr.L <xvxxxy@yandex.ru>
2021-11-07 13:02:04 +0300

    index page cleanup

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,13 @@
 import React from "react";
-import Footer from "../components/footer";
-import IndexHeader from "../components/_prototypes/IndexHeader";
-import WhiteNavbar from "../components/WhiteNavbar";
 import FeaturedProducts from "../components/featuredProducts";
-import SectionCards from "../components/_prototypes/SectionCards";
 import Page from "../components/page";
 
 const Index = () => {
     return (
         <>
-            <WhiteNavbar />
-            <IndexHeader />
             <Page>
                 <FeaturedProducts />
             </Page>
-            <SectionCards />
-            <Footer />
         </>
     );
 };
