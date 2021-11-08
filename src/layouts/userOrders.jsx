1231cba4fd80fcfe7a8890fa8e2b68c26d3baad7
Aleksandr.L <xvxxxy@yandex.ru>
2021-11-08 21:24:09 +0300

    Added userOrders layout

--- src/layouts/userOrders.jsx
+++ src/layouts/userOrders.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import Page from "../components/page";
+import UserOrdersTable from "../components/userOrdersTable";
+import UserOrderDetail from "../components/userOrderDetail";
 
 const UserOrders = () => {
-    return <div>UserOrders</div>;
+    const { orderId } = useParams();
+
+    return (
+        <Page
+            title={orderId ? `Детали по заказу ${orderId}` : "История заказов"}
+        >
+            {orderId ? (
+                <UserOrderDetail orderId={orderId} />
+            ) : (
+                <UserOrdersTable />
+            )}
+        </Page>
+    );
 };
 
 export default UserOrders;
