e486340ab47d5b438c1e7a4abc25a9f74e24d8b9
Aleksandr.L <xvxxxy@yandex.ru>
2021-11-10 22:06:14 +0300

    User components layout change

--- src/layouts/userOrders.jsx
+++ src/layouts/userOrders.jsx
@@ -6,6 +6,28 @@ import UserOrderDetail from "../components/userOrderDetail";
 
 const UserOrders = () => {
     const { orderId } = useParams();
+    const orders = [
+        {
+            _id: "1312313",
+            number: 111,
+            orderNumber: "100",
+            date: "2021",
+            deliveryMethod: "Курьер",
+            paymentMethod: "При получении",
+            status: "Не оплачен",
+            sum: "10000"
+        },
+        {
+            _id: "234234",
+            number: 112,
+            orderNumber: "200",
+            date: "3021",
+            deliveryMethod: "Курьер",
+            paymentMethod: "При получении",
+            status: "Оплачен",
+            sum: "20000"
+        }
+    ];
 
     return (
         <Page
@@ -14,7 +36,7 @@ const UserOrders = () => {
             {orderId ? (
                 <UserOrderDetail orderId={orderId} />
             ) : (
-                <UserOrdersTable />
+                <UserOrdersTable orders={orders} />
             )}
         </Page>
     );
