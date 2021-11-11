aa39fbf4cf592ce84a649ccc2ec109db5985a14b
Aleksandr.L <xvxxxy@yandex.ru>
2021-11-11 22:31:30 +0300

    Added code

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Page from "../components/page";
+import { Button, Col, Row } from "reactstrap";
+import { Link, useHistory } from "react-router-dom";
 
 function Error404(props) {
-    return <div>Error404</div>;
+    const history = useHistory();
+
+    return (
+        <Page title="Oops..404 такой страницы нет">
+            <Row className="text-center ">
+                <Col>
+                    <Button onClick={() => history.goBack()}>Назад</Button>
+                </Col>
+                <Col>
+                    <Button to="/" tag={Link}>
+                        На главную
+                    </Button>
+                </Col>
+            </Row>
+        </Page>
+    );
 }
 
 export default Error404;
