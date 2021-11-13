5940bc987255ef7e0ba26dca7ef9fe7086586b65
Aleksandr.L <xvxxxy@yandex.ru>
2021-11-13 22:32:52 +0300

    Added toast, provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routing/AppRouter";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { CategoriesProvider } from "./hooks/useCategory";
 function App() {
     return (
         <BrowserRouter>
-            <AppRouter />
+            <CategoriesProvider>
+                <AppRouter />
+            </CategoriesProvider>
+            <ToastContainer />
         </BrowserRouter>
     );
 }
