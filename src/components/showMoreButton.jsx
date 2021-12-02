d0e7622572757f0be7a80fedc2bf475615624ae8
Aleksandr.L <xvxxxy@yandex.ru>
2021-12-02 21:25:55 +0300

    Added showMoreButton component

---
+++ src/components/showMoreButton.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button, Col } from "reactstrap";
+
+function ShowMoreButton({ onShowMore }) {
+    return (
+        <Col className="offset-md-4" md="4">
+            <Button
+                className="btn-round"
+                color="default"
+                data-rotation-color="gray"
+                id="successBtn"
+                outline
+                onClick={() => onShowMore()}
+            >
+                Загрузить еще..
+            </Button>
+        </Col>
+    );
+}
+
+export default ShowMoreButton;
