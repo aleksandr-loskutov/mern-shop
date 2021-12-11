70f0c51b09156dfaea5968c9ddbb4bc4f3e273b9
Aleksandr.L <xvxxxy@yandex.ru>
2021-12-11 20:40:46 +0300

    Added cartLayout. Components refactoring

---
+++ src/components/cart.jsx
@@ -0,0 +1,166 @@
+import React from "react";
+import { Button, ButtonGroup, Card, Col, Row, Table } from "reactstrap";
+import { Link } from "react-router-dom";
+const Cart = ({ cartProducts, updateItemQuantity }) => {
+    return (
+        <Card className="mt-4 card-refine">
+            <Row>
+                <Col className="ml-auto mr-auto" md="10">
+                    <Table className="table-shopping" responsive>
+                        <thead>
+                            <tr>
+                                <th className="text-center" colSpan="2">
+                                    Товар
+                                </th>
+                                <th className="text-right">Цена</th>
+                                <th className="text-right">Количество</th>
+                                <th className="text-right">Всего</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {cartProducts?.length > 0 ? (
+                                cartProducts?.map((product) => (
+                                    <tr key={product._id}>
+                                        <td>
+                                            <div className="img-container">
+                                                <Link
+                                                    to={`/product/${product.urlAlias}`}
+                                                >
+                                                    <img
+                                                        alt={product.name}
+                                                        src={
+                                                            require(`assets/img/products/${
+                                                                product.article
+                                                            }/${
+                                                                product.images[0].split(
+                                                                    "/"
+                                                                )[4]
+                                                            }`).default
+                                                        }
+                                                    />
+                                                </Link>
+                                            </div>
+                                        </td>
+                                        <td className="td-product">
+                                            <Link
+                                                to={`/product/${product.urlAlias}`}
+                                            >
+                                                <strong>{product.name}</strong>
+                                            </Link>
+                                            <p>
+                                                {product.description?.length > 0
+                                                    ? product.description.substring(
+                                                          0,
+                                                          100
+                                                      ) + "..."
+                                                    : ""}
+                                            </p>
+                                        </td>
+                                        <td className="td-price">
+                                            {product.price}
+                                        </td>
+                                        <td className="td-number td-quantity">
+                                            {product.cartQuantity}
+                                            <ButtonGroup>
+                                                <Button
+                                                    className="btn-outline-secondary"
+                                                    color="default"
+                                                    size="sm"
+                                                    onClick={() =>
+                                                        updateItemQuantity(
+                                                            product._id,
+                                                            product.cartQuantity -
+                                                                1
+                                                        )
+                                                    }
+                                                >
+                                                    -
+                                                </Button>
+                                                <Button
+                                                    className="btn-outline-secondary"
+                                                    color="secondary"
+                                                    size="sm"
+                                                    onClick={() =>
+                                                        updateItemQuantity(
+                                                            product._id,
+                                                            product.cartQuantity +
+                                                                1
+                                                        )
+                                                    }
+                                                >
+                                                    +
+                                                </Button>
+                                            </ButtonGroup>
+                                        </td>
+                                        <td className="td-number">
+                                            {product.cartQuantity *
+                                                product.price}{" "}
+                                            <small className="font-weight-bold">
+                                                ₽
+                                            </small>
+                                        </td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan="5" className="text-center">
+                                        <h3 className="text-secondary font-weight-bold mb-4">
+                                            В корзине пусто :(
+                                        </h3>
+                                        <Button
+                                            color="danger"
+                                            className="btn-lg btn-round"
+                                            to="/catalog/"
+                                            tag={Link}
+                                        >
+                                            ЗА ПОКУПКАМИ
+                                        </Button>
+                                    </td>
+                                </tr>
+                            )}
+                            {cartProducts.length > 0 && (
+                                <>
+                                    <tr>
+                                        <td colSpan="2" />
+                                        <td />
+                                        <td className="td-total">Итого</td>
+                                        <td className="td-total">
+                                            {cartProducts.reduce(
+                                                (acc, product) =>
+                                                    acc +
+                                                    product.price *
+                                                        product.cartQuantity,
+                                                0
+                                            )}{" "}
+                                            <small className="font-weight-bold">
+                                                ₽
+                                            </small>
+                                        </td>
+                                    </tr>
+
+                                    <tr className="tr-actions">
+                                        <td colSpan="3" />
+                                        <td className="text-right" colSpan="2">
+                                            <Button
+                                                color="danger"
+                                                size="lg"
+                                                type="button"
+                                                to="/cart/checkout"
+                                                tag={Link}
+                                            >
+                                                ЗАКАЗАТЬ{" "}
+                                                <i className="fa fa-chevron-right" />
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                </>
+                            )}
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
+        </Card>
+    );
+};
+
+export default Cart;
---
+++ src/layouts/cartLayout.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import CheckOut from "../components/checkOut";
+import Cart from "../components/cart";
+import Page from "../components/page";
+import { useCart } from "react-use-cart";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../store/actions/products";
+import Preloader from "../components/preloader";
+
+function CartLayout() {
+    const { checkout } = useParams();
+    const [cartProducts, setCartProducts] = useState([]);
+    const { isEmpty, items, updateItemQuantity } = useCart();
+    const { products } = useSelector((state) => state.products);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, [dispatch]);
+    useEffect(() => {
+        if (products?.content?.length > 0) {
+            setCartProducts(
+                !isEmpty
+                    ? items.reduce((acc, item) => {
+                          const productFromDb = products.content.find(
+                              (p) => p._id === item.id
+                          );
+                          if (productFromDb)
+                              acc.push({
+                                  ...productFromDb,
+                                  cartQuantity: item.quantity
+                              });
+                          return acc;
+                      }, [])
+                    : []
+            );
+            if (!isLoaded) {
+                setIsLoaded(true);
+            }
+        }
+    }, [products, items]);
+    return (
+        <Page title={checkout ? "Оформление заказа" : "Корзина"}>
+            {isLoaded ? (
+                checkout ? (
+                    <CheckOut cartProducts={cartProducts} />
+                ) : (
+                    <Cart
+                        cartProducts={cartProducts}
+                        updateItemQuantity={updateItemQuantity}
+                    />
+                )
+            ) : (
+                <Preloader blockClass="mt-5" />
+            )}
+        </Page>
+    );
+}
+
+export default CartLayout;
