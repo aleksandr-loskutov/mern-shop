d1246e1fb617f064c00ca1ee4084ef9a5b230af3
Aleksandr.L <xvxxxy@yandex.ru>
2021-12-13 22:45:12 +0300

    Moved

--- src/layouts/loginLayout.jsx
+++ src/layouts/loginLayout.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Page from "../../components/page";
-import LoginForm from "../../components/form/loginForm";
+import Page from "../components/page";
+import LoginForm from "../components/form/loginForm";
 import { useParams } from "react-router-dom";
-import RegisterForm from "../../components/form/registerForm";
+import RegisterForm from "../components/form/registerForm";
 
-function LoginPage() {
+function LoginLayout() {
     const { register } = useParams();
     return (
         <>
@@ -19,4 +19,4 @@ function LoginPage() {
     );
 }
 
-export default LoginPage;
+export default LoginLayout;
