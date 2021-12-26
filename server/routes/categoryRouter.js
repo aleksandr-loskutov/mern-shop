215586d2b5f13b8f9f2d5e9de0a4c186941ffabb
Aleksandr.L <xvxxxy@yandex.ru>
2021-12-26 22:55:49 +0300

    Added image upload middleware

--- server/routes/categoryRouter.js
+++ server/routes/categoryRouter.js
@@ -2,6 +2,7 @@ const Router = require("express");
 const roleMiddleware = require("../middleware/roleMiddleware");
 const categoryController = require("../controllers/categoryController");
 const { check } = require("express-validator");
+const upload = require("../middleware/upload");
 const router = new Router();
 router.post(
     "/",
@@ -13,10 +14,16 @@ router.post(
         min: 4,
         max: 50
     }),
+    upload.single("image"),
     categoryController.create
+);
+router.patch(
+    "/:id",
+    roleMiddleware(["admin"]),
+    upload.single("image"),
+    categoryController.update
 );
 router.get("/", categoryController.getAll);
 router.get("/:id", categoryController.getOne);
-router.patch("/:id", roleMiddleware(["admin"]), categoryController.update);
 router.delete("/:id", roleMiddleware(["admin"]), categoryController.delete);
 module.exports = router;
