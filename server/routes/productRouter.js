d6f976627b3da2ae8ac3e46c6dc1edaaad65a069
Aleksandr.L <xvxxxy@yandex.ru>
2021-12-27 23:54:13 +0300

    Added image upload middleware

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -2,26 +2,27 @@ const Router = require("express");
 const productController = require("../controllers/productController");
 const roleMiddleware = require("../middleware/roleMiddleware");
 const { check } = require("express-validator");
+const upload = require("../middleware/upload");
 const router = new Router();
-router.post(
-    "/",
-    [
-        roleMiddleware(["admin"]),
-        check(
-            "name",
-            "Наименование товара должно быть от 4 до 50 символов"
-        ).isLength({
-            min: 4,
-            max: 50
-        }),
-        check("price", "Цена не указана").isNumeric()
-    ],
-    productController.create
+router.post("/", [upload.single("image")], productController.create);
+//roleMiddleware(["admin"]),
+router.patch(
+    "/:id",
+    roleMiddleware(["admin"]),
+    upload.single("image"),
+    productController.update
 );
-router.patch("/:id", roleMiddleware(["admin"]), productController.update);
 router.delete("/:id", roleMiddleware(["admin"]), productController.delete);
 router.get("/", productController.getAll);
 router.get("/:id", productController.getOne);
 router.get("/cat/:categoryId", productController.getByCategory);
 
 module.exports = router;
+
+// check(
+//     "name",
+//     "Наименование товара должно быть от 4 до 50 символов"
+// ).isLength({
+//     min: 4,
+//     max: 50
+// }),
