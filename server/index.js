53f8b429b40bad5a0c475a209e210428813ee7b1
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-07 22:16:57 +0300

    Added initDatabase, env, urlencoded

--- server/index.js
+++ server/index.js
@@ -2,20 +2,31 @@ const express = require("express");
 const db = require("./models/index");
 const config = require("config");
 const authRouter = require("./routes/authRouter");
-const PORT = config.get("serverPort");
+const initDatabase = require("./startup/initDatabase");
+const PORT = config.get("serverPort") ?? 4000;
 const DB_URL = config.get("dbUrl");
 const app = express();
 const cors = require("cors");
 const router = require("./routes/index");
 const errorHandler = require("./middleware/errorHandler");
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use("/api", router);
 app.use("/uploads", express.static("uploads"));
 app.use(errorHandler);
 
+if (process.env.NODE_ENV === "production") {
+    console.log("Production");
+} else {
+    console.log("Development");
+}
+
 const start = async () => {
     try {
+        db.mongoose.connection.once("open", () => {
+            initDatabase();
+        });
         await db.mongoose.connect(DB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
