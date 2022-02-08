89788edf53e05ff926f865b58efa11ad09942879
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-08 19:44:14 +0300

    Added createAdminIfNotExist function

--- server/startup/initDatabase.js
+++ server/startup/initDatabase.js
@@ -1,8 +1,13 @@
 const Product = require("../models/product");
 const Category = require("../models/category");
+const User = require("../models/user");
 const productsMock = require("../mockData/products.json");
 const categoriesMock = require("../mockData/categories.json");
+const bcrypt = require("bcrypt");
+const config = require("config");
+const chalk = require("chalk");
 module.exports = async () => {
+    if (!config.get("importMockData")) return;
     const categories = await Category.find();
     if (categories.length !== categoriesMock.length) {
         console.log(
@@ -27,6 +32,31 @@ module.exports = async () => {
         await Product.collection.drop();
         const result = await createInitialProductsEntity(Product, productsMock);
         console.log(`Импорт товаров завершен`);
+    }
+
+    if (config.get("createAdminIfNotExist")) {
+        const admin = await User.findOne({ role: "admin" });
+        if (!admin) {
+            const user = new User({
+                email: config.get("admin").email,
+                password: bcrypt.hashSync(config.get("admin").password, 7),
+                role: "admin"
+            });
+            await user.save();
+            console.log(
+                chalk.green.inverse(
+                    `Создан новый админ согласно конфигу (/config/default.json): ${
+                        config.get("admin").email
+                    } с паролем  ${config.get("admin").password}`
+                )
+            );
+        }
+    }
+    if (
+        products.length === productsMock.length &&
+        categories.length === categoriesMock.length
+    ) {
+        console.log(chalk.green.inverse("Мок данные актуальны"));
     }
 };
 
