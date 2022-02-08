ba2f14cfb058c5add9a89b69d4e72397bccdc3fe
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-08 19:45:22 +0300

    Added /token route & controller

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -15,7 +15,17 @@ router.post(
     ],
     controller.registration
 );
-router.post("/login", controller.login);
+router.post(
+    "/login",
+    [
+        check("email", "Введите корректный Email").normalizeEmail().isEmail(),
+        check("password", "Пароль должен содержать от 4 символов").isLength({
+            min: 4
+        })
+    ],
+    controller.login
+);
 router.post("/users", roleMiddleware(["admin"]), controller.getUsers);
+router.post("/token", controller.token);
 router.get("/auth", authMiddleware, controller.checkAuth);
 module.exports = router;
