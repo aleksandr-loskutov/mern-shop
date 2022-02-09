ab1f78de0ab0d423b273f893cf88cf041c441424
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-09 20:22:12 +0300

    Removed cart router, model, controller  (at least for now)

--- server/routes/cartRouter.js
+++
@@ -1,6 +0,0 @@
-const Router = require("express");
-const cartController = require("../controllers/cartController");
-const router = new Router();
-router.get("/:id", cartController.get);
-router.patch("/:id", cartController.update);
-module.exports = router;
