4569bc129ae1f59c30bcf7aa3dfdaa69e4433822
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-11 22:13:12 +0300

    Changed model

--- server/models/order.js
+++ server/models/order.js
@@ -2,10 +2,11 @@ const { Schema, model, ObjectId } = require("mongoose");
 const Order = new Schema(
     {
         userId: { type: ObjectId, ref: "User", required: true },
+        orderNumber: { type: Number, required: true },
+        receiver: { type: Object, required: true },
         products: { type: Array, required: true },
         total: { type: Number, required: true },
-        payment: { type: Boolean, required: true, default: false },
-        comment: { type: String }
+        payment: { type: Boolean, required: true, default: false }
     },
     { timestamps: true }
 );
