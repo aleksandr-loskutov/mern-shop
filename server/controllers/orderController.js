887141e7e9c95d2ae0abbf455002291ad7f58040
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-11 22:14:18 +0300

    Added new order controller

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,12 +1,10 @@
 const Order = require("../models/order");
-const Address = require("../models/address");
 const { validationResult } = require("express-validator");
-const Category = require("../models/category");
-const Cart = require("../models/cart");
+const Product = require("../models/product");
+const User = require("../models/user");
 class OrderController {
     async create(req, res) {
         try {
-            //TODO adds address to user if not exist, modify if exist
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
                 return res.status(400).json({
@@ -15,30 +13,76 @@ class OrderController {
                     errors
                 });
             }
-            const { userId, cartId, comment } = req.body;
-            // TODO расчет total
-            let cart = await Cart.findOne({ _id: cartId });
-            let products = await Cart.find({ _id: cart.products });
+            const { products } = req.body;
+            //find products by ids to trust further
+            const dbProductsForOrder = await Product.find()
+                .where("_id")
+                .in(
+                    products.reduce((acc, product) => {
+                        acc.push(product._id);
+                        return acc;
+                    }, [])
+                )
+                .exec();
+            if (!dbProductsForOrder || dbProductsForOrder.length === 0) {
+                return res.status(400).json({
+                    status: 400,
+                    message: "Ошибка при добавлении заказа",
+                    errors
+                });
+            }
+            //return back cartQuantity from user products array
+            const orderedProducts = dbProductsForOrder.map((product) => {
+                const index = products.findIndex((p) =>
+                    product._id.equals(p._id)
+                );
+                return {
+                    ...product["_doc"],
+                    cartQuantity: products[index].cartQuantity
+                };
+            });
 
-            // const { products } = cart;
-            const sum = products.reduce(
-                (acc, product) => acc + product.price,
-                0
-            );
+            const extraPrice = req.body.extraPrice || 0;
+            const total =
+                orderedProducts.reduce(
+                    (acc, product) =>
+                        acc + product.price * product.cartQuantity,
+                    0
+                ) + extraPrice;
+
+            const userId = req.user._id;
+            const receiver = {
+                name: req.body.name,
+                lastName: req.body.lastName,
+                phone: req.body.phone,
+                city: req.body.city,
+                address: req.body.address,
+                postCode: req.body.postCode
+            };
             const order = new Order({
                 userId: userId,
-                cartId: cartId,
-                total: sum,
-                comment: comment
+                orderNumber: Date.now(),
+                deliveryMethod: req.body.deliveryMethod,
+                receiver,
+                products: orderedProducts,
+                total,
+                payment: req.body.paymentType === "card"
+            });
+
+            const updatedUserReceiver = await User.findByIdAndUpdate(userId, {
+                receiver
             });
             await order.save();
-            //TODO сгенерировать новую корзину для пользователя
             return res.status(200).json({
                 status: 200,
                 message: "Успешно добавлен заказ",
                 content: order
             });
-        } catch (e) {}
+        } catch (e) {
+            res.status(500).json({
+                message: "На сервере возникла ошибка. Попробуйте позже."
+            });
+        }
     }
     async getAll(req, res) {
         try {
@@ -49,7 +93,11 @@ class OrderController {
                 content: orders,
                 message: "Successfully orders retrieved"
             });
-        } catch (e) {}
+        } catch (e) {
+            res.status(500).json({
+                message: "На сервере возникла ошибка. Попробуйте позже."
+            });
+        }
     }
     async getOne(req, res) {
         try {
@@ -63,12 +111,23 @@ class OrderController {
                 : res
                       .status(404)
                       .json({ status: 404, message: "order not found" });
-        } catch (e) {}
+        } catch (e) {
+            res.status(500).json({
+                message: "На сервере возникла ошибка. Попробуйте позже."
+            });
+        }
     }
     async getByUser(req, res) {
         //user orders, check user id in token and compare it with request id
         try {
-        } catch (e) {}
+        } catch (e) {
+            res.status(500).json({
+                message: "На сервере возникла ошибка. Попробуйте позже."
+            });
+        }
     }
+}
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 module.exports = new OrderController();
