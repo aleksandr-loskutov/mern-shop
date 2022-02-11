ade4617af5a85e38f4acb14cc164a39693538c14
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-11 22:08:00 +0300

    Replaced router, added history

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
 import AppRouter from "./routing/AppRouter";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { CartProvider } from "react-use-cart";
 import AppLoader from "./components/hoc/appLoader";
+import history from "./utils/history";
 function App() {
     return (
-        <BrowserRouter>
+        <Router history={history}>
             <CartProvider>
                 <AppLoader>
                     <AppRouter />
                 </AppLoader>
             </CartProvider>
             <ToastContainer />
-        </BrowserRouter>
+        </Router>
     );
 }
 
