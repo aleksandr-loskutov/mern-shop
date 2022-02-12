7d1279070027b9806cd58cc6ccf588b121127db0
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-12 23:00:11 +0300

    Added redux, history, error

--- src/components/form/loginForm.jsx
+++ src/components/form/loginForm.jsx
@@ -3,26 +3,28 @@ import { Button, Card, CardTitle, Col, Container, Row } from "reactstrap";
 import FormComponent, { TextField } from "./index";
 import { Link } from "react-router-dom";
 import * as yup from "yup";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getAuthErrors, login } from "../../store/users";
+
 const LoginForm = () => {
-    const handleSubmit = (data) => {
-        console.log("data", data);
-        console.log("success");
+    const loginError = useSelector(getAuthErrors());
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const handleSubmit = async (data) => {
+        console.log("login data", data);
+        const redirect = history.location.state
+            ? history.location.state.from.pathname
+            : "/catalog";
+        dispatch(login({ payload: data, redirect }));
     };
     const validateSchema = yup.object().shape({
         password: yup
             .string()
             .required("пароль обязателен")
             .matches(
-                /(?=.*[A-Z])/,
-                "Пароль должен содержать как минимум 1 заглавную букву"
-            )
-            .matches(
                 /(?=.*[0-9])/,
                 "Пароль должен содержать как минимум 1 число"
-            )
-            .matches(
-                /(?=.*[!@#$%^&*])/,
-                "Пароль должен содержать как минимум 1 спецсимвол из @#$%^&*"
             )
             .matches(/(?=.{8,})/, "Пароль должен быть минимум 8 символов"),
         email: yup
@@ -60,6 +62,9 @@ const LoginForm = () => {
                                 Вход
                             </Button>
                         </FormComponent>
+                        {loginError && (
+                            <p className="text-danger  mt-2">{loginError}</p>
+                        )}
                         <div className="login m-2">
                             <p>
                                 Нет аккаунта?{" "}
