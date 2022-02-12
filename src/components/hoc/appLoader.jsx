7635c0b7eb970472543859123797cc8e673aa8cb
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-12 22:48:13 +0300

    Added isLoggedIn logic

--- src/components/hoc/appLoader.jsx
+++ src/components/hoc/appLoader.jsx
@@ -1,8 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// import {
-//     getIsLoggedIn,
-// } from "../../store/users";
+import { getIsLoggedIn, loadUsersList } from "../../store/users";
+import { loadOrdersList } from "../../store/orders";
 import PropTypes from "prop-types";
 import {
     loadCategoriesList,
@@ -14,18 +13,18 @@ import {
 } from "../../store/products";
 function AppLoader({ children }) {
     const dispatch = useDispatch();
-    // const isLoggedIn = useSelector(getIsLoggedIn());
+    const isLoggedIn = useSelector(getIsLoggedIn());
     const isCategoriesLoading = useSelector(getCategoriesLoadingStatus());
     const isProductsLoading = useSelector(getProductsLoadingStatus());
     const isLoaded = !isCategoriesLoading && !isProductsLoading;
     useEffect(() => {
         dispatch(loadCategoriesList());
         dispatch(loadProductsList());
-        // if (isLoggedIn) {
-        //     dispatch(loadUsersList());
-        // }
-        // если используем  isLoggedIn добавить эту зависимость в массив
-    }, [dispatch]);
+        if (isLoggedIn) {
+            dispatch(loadUsersList());
+            dispatch(loadOrdersList());
+        }
+    }, [dispatch, isLoggedIn]);
     if (!isLoaded) return "Loading...";
     return children;
 }
