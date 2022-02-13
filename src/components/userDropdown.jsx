e256fe714ff3cf7e34f0bca6bc24a68517e75bc7
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-13 22:52:39 +0300

    Added user dropdown logic & admin panel link

--- src/components/userDropdown.jsx
+++ src/components/userDropdown.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import {
+    Button,
     DropdownItem,
     DropdownMenu,
     DropdownToggle,
+    NavItem,
     UncontrolledDropdown
 } from "reactstrap";
 import { Link } from "react-router-dom";
@@ -19,42 +21,56 @@ function UserDropdown(props) {
     const isLoading = useSelector(getUsersLoadingStatus());
     if (isLoading) return "Loading...";
     return (
-        <UncontrolledDropdown nav inNavbar>
-            <DropdownToggle color="default" caret nav>
-                {currentUser
-                    ? currentUser.email.split("@")[0].substring(0, 15)
-                    : "Личный кабинет"}
-            </DropdownToggle>
-            <DropdownMenu className="dropdown-danger" right>
-                {!currentUser ? (
-                    <>
-                        <DropdownItem to="/login" tag={Link}>
-                            <i className="nc-icon nc-bullet-list-67" />
-                            Вход
-                        </DropdownItem>
-                        <DropdownItem to="/login/register" tag={Link}>
-                            <i className="nc-icon nc-bullet-list-67" />
-                            Регистрация
-                        </DropdownItem>
-                    </>
-                ) : (
-                    <>
-                        <DropdownItem to="/user/orders/" tag={Link}>
-                            <i className="nc-icon nc-app" />
-                            Мои заказы
-                        </DropdownItem>
-                        <DropdownItem to="/user" tag={Link}>
-                            <i className="nc-icon nc-badge" />
-                            Мои данные
-                        </DropdownItem>
-                        <DropdownItem to="/logout" tag={Link}>
-                            <i className="nc-icon nc-button-power" />
-                            Выйти
-                        </DropdownItem>
-                    </>
-                )}
-            </DropdownMenu>
-        </UncontrolledDropdown>
+        <>
+            <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle color="default" caret nav>
+                    {currentUser
+                        ? currentUser.email.split("@")[0].substring(0, 15)
+                        : "Личный кабинет"}
+                </DropdownToggle>
+                <DropdownMenu className="dropdown-danger" right>
+                    {!currentUser ? (
+                        <>
+                            <DropdownItem to="/login" tag={Link}>
+                                <i className="nc-icon nc-bullet-list-67" />
+                                Вход
+                            </DropdownItem>
+                            <DropdownItem to="/login/register" tag={Link}>
+                                <i className="nc-icon nc-bullet-list-67" />
+                                Регистрация
+                            </DropdownItem>
+                        </>
+                    ) : (
+                        <>
+                            <DropdownItem to="/user/orders/" tag={Link}>
+                                <i className="nc-icon nc-app" />
+                                Мои заказы
+                            </DropdownItem>
+                            <DropdownItem to="/user" tag={Link}>
+                                <i className="nc-icon nc-badge" />
+                                Мои данные
+                            </DropdownItem>
+                            <DropdownItem to="/logout" tag={Link}>
+                                <i className="nc-icon nc-button-power" />
+                                Выйти
+                            </DropdownItem>
+                        </>
+                    )}
+                </DropdownMenu>
+            </UncontrolledDropdown>
+            {role === "admin" && (
+                <NavItem>
+                    <Button
+                        className="btn-round"
+                        color="danger"
+                        tag={Link}
+                        to={"/admin"}
+                    >
+                        <i className="nc-icon nc-key-25" /> Админ
+                    </Button>
+                </NavItem>
+            )}
+        </>
     );
 }
 
