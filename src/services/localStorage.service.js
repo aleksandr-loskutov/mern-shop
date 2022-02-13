820930e5cc8ab46820b2f8a29402a2443e3e5d62
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-13 22:44:19 +0300

    Added role

--- src/services/localStorage.service.js
+++ src/services/localStorage.service.js
@@ -2,15 +2,18 @@ const TOKEN_KEY = "jwt-token";
 const REFRESH_KEY = "jwt-refresh-token";
 const EXPIRES_KEY = "jwt-expires";
 const USERID_KEY = "user-local-id";
+const USER_ROLE = "user-role";
 
 export function setTokens({
     refreshToken,
     accessToken,
     userId,
+    role,
     expiresIn = 3600
 }) {
     const expiresDate = new Date().getTime() + expiresIn * 1000;
     localStorage.setItem(USERID_KEY, userId);
+    localStorage.setItem(USER_ROLE, role);
     localStorage.setItem(TOKEN_KEY, accessToken);
     localStorage.setItem(REFRESH_KEY, refreshToken);
     localStorage.setItem(EXPIRES_KEY, expiresDate);
@@ -23,6 +26,7 @@ export function getRefreshToken() {
 }
 export function removeAuthData() {
     localStorage.removeItem(USERID_KEY);
+    localStorage.removeItem(USER_ROLE);
     localStorage.removeItem(TOKEN_KEY);
     localStorage.removeItem(REFRESH_KEY);
     localStorage.removeItem(EXPIRES_KEY);
@@ -34,12 +38,16 @@ export function getTokenExpiresDate() {
 export function getUserId() {
     return localStorage.getItem(USERID_KEY);
 }
+export function getUserRole() {
+    return localStorage.getItem(USER_ROLE);
+}
 const localStorageService = {
     setTokens,
     getAccessToken,
     getRefreshToken,
     getTokenExpiresDate,
     getUserId,
+    getUserRole,
     removeAuthData
 };
 export default localStorageService;
