20cad069c72324ab30df91fb7af8b06e2fe0e86e
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-13 22:45:48 +0300

    Added deliveryMethod & status

--- server/models/order.js
+++ server/models/order.js
@@ -2,11 +2,13 @@ const { Schema, model, ObjectId } = require("mongoose");
 const Order = new Schema(
     {
         userId: { type: ObjectId, ref: "User", required: true },
+        deliveryMethod: { type: String, required: true },
         orderNumber: { type: Number, required: true },
         receiver: { type: Object, required: true },
         products: { type: Array, required: true },
         total: { type: Number, required: true },
-        payment: { type: Boolean, required: true, default: false }
+        payment: { type: Boolean, required: true, default: false },
+        status: { type: String, default: "создан" }
     },
     { timestamps: true }
 );
