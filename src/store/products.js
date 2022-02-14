277ec9d8ee70a3eb45493604113b7a28c75b9948
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-14 22:21:17 +0300

    Added product posting

--- src/store/products.js
+++ src/store/products.js
@@ -1,5 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAction, createSlice } from "@reduxjs/toolkit";
 import productService from "../services/product.service";
+import isOutdated from "../utils/isOutdated";
+import history from "../utils/history";
 
 const productsSlice = createSlice({
     name: "products",
@@ -18,22 +20,37 @@ const productsSlice = createSlice({
             state.lastFetch = Date.now();
             state.isLoading = false;
         },
-        productsRequestFiled: (state, action) => {
+        productsRequestFailed: (state, action) => {
             state.error = action.payload;
             state.isLoading = false;
+        },
+        productAdded: (state, action) => {
+            state.entities.push(action.payload);
+        },
+        productAddFailed: (state, action) => {
+            state.error = action.payload;
+        },
+        productDeleted: (state, action) => {
+            state.entities = state.entities.filter(
+                (c) => c._id !== action.payload
+            );
+        },
+        productDeleteFailed: (state, action) => {
+            state.error = action.payload;
         }
     }
 });
-
 const { reducer: productsReducer, actions } = productsSlice;
-const { productsRequested, productsReceved, productsRequestFiled } = actions;
-
-function isOutdated(date) {
-    if (Date.now() - date > 10 * 60 * 100) {
-        return true;
-    }
-    return false;
-}
+const {
+    productsRequested,
+    productsReceved,
+    productsRequestFailed,
+    productAdded,
+    productAddFailed,
+    productDeleted,
+    productDeleteFailed
+} = actions;
+const productRequestAdd = createAction("products/addRequest");
 
 export const loadProductsList = () => async (dispatch, getState) => {
     const { lastFetch } = getState().products;
@@ -43,14 +60,32 @@ export const loadProductsList = () => async (dispatch, getState) => {
             const { content } = await productService.fetchAll();
             dispatch(productsReceved(content));
         } catch (error) {
-            dispatch(productsRequestFiled(error.message));
+            dispatch(productsRequestFailed(error.message));
         }
     }
 };
 
+export const addProduct = (payload) => async (dispatch) => {
+    dispatch(productRequestAdd());
+    try {
+        const { content } = await productService.create(payload);
+        //console.log('content', content)
+        dispatch(productAdded(content));
+        history.push("/admin/products/");
+    } catch (error) {
+        const { message } = error.response?.data;
+        const { status: code } = error.response;
+        if (code === 400) {
+            dispatch(productAddFailed(message));
+        } else {
+            dispatch(productAddFailed(error.message));
+        }
+    }
+};
 export const getProducts = () => (state) => state.products.entities;
 export const getProductsLoadingStatus = () => (state) =>
     state.products.isLoading;
+export const getProductsErrors = () => (state) => state.products.error;
 export const getProductsByIds = (productsIds) => (state) => {
     if (state.products.entities) {
         const productsArray = [];
@@ -70,6 +105,14 @@ export const getProductByAlias = (alias) => (state) => {
     if (state.products.entities) {
         return state.products.entities[
             state.products.entities.findIndex((p) => p.urlAlias === alias)
+        ];
+    }
+    return {};
+};
+export const getProductById = (productId) => (state) => {
+    if (state.products.entities) {
+        return state.products.entities[
+            state.products.entities.findIndex((p) => p._id === productId)
         ];
     }
     return {};
