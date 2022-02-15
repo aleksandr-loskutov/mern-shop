47a09a61a352779e48182ec757feeaa3cc1313d0
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-15 22:02:15 +0300

    Added basic checks for now

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -4,21 +4,33 @@ const roleMiddleware = require("../middleware/roleMiddleware");
 const { check } = require("express-validator");
 const upload = require("../middleware/upload");
 const router = new Router();
-router.post("/", [upload.single("image")], productController.create);
-//roleMiddleware(["admin"]),
+const productValidations = [
+    check("name", "Укажите название").notEmpty(),
+    check("categoryId", "Укажите категорию").notEmpty(),
+    check("stock", "Укажите наличие").notEmpty(),
+    check("price", "Укажите цену").notEmpty(),
+    check("article", "Укажите артикул").notEmpty()
+];
+router.post(
+    "/",
+    roleMiddleware(["admin"]),
+    upload.single("image"),
+    productValidations,
+    productController.create
+);
 router.patch(
     "/:id",
     roleMiddleware(["admin"]),
     upload.single("image"),
+    productValidations,
     productController.update
 );
 router.delete("/:id", roleMiddleware(["admin"]), productController.delete);
 router.get("/", productController.getAll);
-router.get("/:id", productController.getOne);
-router.get("/cat/:categoryId", productController.getByCategory);
 
 module.exports = router;
-
+// router.get("/:id", productController.getOne);
+// router.get("/cat/:categoryId", productController.getByCategory);
 // check(
 //     "name",
 //     "Наименование товара должно быть от 4 до 50 символов"
