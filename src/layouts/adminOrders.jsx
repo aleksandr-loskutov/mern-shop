4889759d23786e1798f885c4ef585cd9edde758d
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-16 23:02:54 +0300

    Added redux data flow

--- src/layouts/adminOrders.jsx
+++ src/layouts/adminOrders.jsx
@@ -1,49 +1,21 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import PageAdmin from "../components/pageAdmin";
-import AdminOrderDetail from "../components/adminOrderDetail";
 import AdminOrdersTable from "../components/table/adminOrderTable";
+import { useSelector } from "react-redux";
+import { getOrderByNumber, getOrders } from "../store/orders";
+import OrderDetail from "../components/orderDetail";
 
 const AdminOrders = () => {
     const { orderId } = useParams();
-    const orders = [
-        {
-            _id: "1312313",
-            number: 111,
-            orderNumber: "100",
-            date: "2021",
-            deliveryMethod: "Курьер",
-            paymentMethod: "При получении",
-            status: "Не оплачен",
-            sum: "10000"
-        },
-        {
-            _id: "234234",
-            number: 112,
-            orderNumber: "200",
-            date: "3021",
-            deliveryMethod: "Курьер",
-            paymentMethod: "При получении",
-            status: "Оплачен",
-            sum: "20000"
-        }
-    ];
-    const order = {
-        _id: "1312313",
-        number: 111,
-        orderNumber: "100",
-        date: "2021",
-        deliveryMethod: "Курьер",
-        paymentMethod: "При получении",
-        status: "Не оплачен",
-        sum: "10000"
-    };
+    const orders = useSelector(getOrders());
+    const order = useSelector(getOrderByNumber(orderId));
     return (
         <PageAdmin
             title={orderId ? `Детали по заказу ${orderId}` : "Все заказы"}
         >
             {orderId ? (
-                <AdminOrderDetail order={order} />
+                <OrderDetail order={order} />
             ) : (
                 <AdminOrdersTable orders={orders} />
             )}
