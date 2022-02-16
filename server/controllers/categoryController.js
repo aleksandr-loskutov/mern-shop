da00a54532b3b754ea59a940f7f1fba1c8975643
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-16 23:06:57 +0300

    Refactoring

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,9 +1,8 @@
 const Category = require("../models/category");
 const { validationResult } = require("express-validator");
-const { sanitize } = require("string-sanitizer");
-const cyrillicToTranslit = require("cyrillic-to-translit-js");
-const Product = require("../models/product");
-
+const { json } = require("express");
+const getUrlFormString = require("../utils/getUrlFromString");
+//
 class CategoryController {
     async create(req, res) {
         try {
@@ -14,92 +13,128 @@ class CategoryController {
                     message: "Ошибка при добавлении категории",
                     errors
                 });
-            }
-            const { name, description, file, metaTitle, categoryId } = req.body;
-            const alias = sanitize.addDash(
-                cyrillicToTranslit().transform(name).toLowerCase()
-            );
-            const checkForName = await Category.findOne({ name: name });
-            if (checkForName) {
-                return res.status(400).json({
-                    status: 400,
-                    message:
-                        "Категория с таким наименованием / URL алиасом уже есть в базе"
+            } else {
+                const { name } = req.body;
+                const alias = getUrlFormString(name);
+                const checkForName = await Category.findOne({ name: name });
+                const checkForAlias = await Category.findOne({
+                    urlAlias: alias
+                });
+                const root = await Category.findOne({ name: "root" });
+                const parentId = root._id;
+                if (checkForName || checkForAlias) {
+                    return res.status(400).json({
+                        status: 400,
+                        message:
+                            "Категория с таким наименованием или url уже есть в базе",
+                        errors
+                    });
+                }
+                const category = new Category({
+                    ...req.body,
+                    parentId,
+                    img: req.file ? "/images/uploads/" + req.file.filename : "",
+                    urlAlias: alias
+                });
+                await category.save();
+                return res.status(201).json({
+                    message: "Успешно добавлена категория",
+                    status: 201,
+                    content: category
                 });
             }
-            let parentCat = categoryId;
-            if (!parentCat || parentCat.isEmpty()) {
-                let root = await Category.findOne({ name: "root" });
-
-                if (!root) {
-                    root = new Category({ name: "root" });
-                    await root.save();
-                }
-                parentCat = root._id;
-            }
-            const category = new Category({
-                name: name,
-                description: description,
-                parentId: parentCat,
-                metaTitle: metaTitle,
-                img: file ? file.path : "",
-                urlAlias: alias
+        } catch (e) {
+            res.status(500).json({
+                message: "На сервере возникла ошибка. Попробуйте позже.",
+                error: e.message
             });
-            await category.save();
-            return res.status(200).json({
-                status: 200,
-                message: "Успешно добавлена категория",
-                content: category
-            });
-        } catch (e) {}
+        }
     }
     async getAll(req, res) {
         try {
             const categories = await Category.find({ name: { $ne: "root" } });
-            // console.log("request for getAll categories");
             return res.status(200).json({
+                message: "Получены категории",
                 status: 200,
-                content: categories,
-                message: "Successfully categories retrieved"
+                content: categories
             });
-        } catch (e) {}
+        } catch (e) {
+            res.status(500).json({
+                message: "На сервере возникла ошибка. Попробуйте позже.",
+                error: e.message
+            });
+        }
     }
-    async getOne(req, res) {
-        try {
-            const category = await Category.findById(req.params.id);
-            return category
-                ? res.status(200).json({
-                      status: 200,
-                      content: category,
-                      message: "Successfully category retrieved"
-                  })
-                : res
-                      .status(404)
-                      .json({ status: 404, message: "Категория не найдена" });
-        } catch (e) {}
-    }
+
     async update(req, res) {
         try {
-            const category = await Category.findOneAndUpdate(
-                { _id: req.params.id },
-                { $set: req.body },
-                { new: true }
-            );
-            res.status(200).json({
-                status: 200,
-                content: category,
-                message: "Successfully category updated"
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    status: 400,
+                    message: "Ошибка при добавлении категории",
+                    errors
+                });
+            } else {
+                const { name } = req.body;
+                const categoryId = req.params.id;
+                const dbCategory = await Category.findById(categoryId);
+                let alias = getUrlFormString(name);
+                if (dbCategory.name !== name && dbCategory.urlAlias !== alias) {
+                    const checkForAlias = await Category.findOne({
+                        urlAlias: alias
+                    });
+                    if (checkForAlias) {
+                        return res.status(400).json({
+                            status: 400,
+                            message:
+                                "Категория с таким наименованием или url уже есть в базе"
+                        });
+                    }
+                }
+
+                const editedData = {
+                    ...req.body,
+                    urlAlias: alias,
+                    img: req.file
+                        ? "/images/uploads/" + req.file.filename
+                        : dbCategory.img
+                };
+                // console.log("editedData", editedData);
+                const category = await Category.findOneAndUpdate(
+                    { _id: categoryId },
+                    {
+                        $set: editedData
+                    },
+                    { new: true }
+                );
+                res.status(200).json({
+                    message: "Успешно",
+                    content: category,
+                    status: 200
+                });
+            }
+        } catch (e) {
+            res.status(500).json({
+                message: "На сервере возникла ошибка. Попробуйте позже.",
+                error: e.message
             });
-        } catch (e) {}
+        }
     }
     async delete(req, res) {
         try {
             await Category.deleteOne({ _id: req.params.id });
             res.status(200).json({
                 status: 200,
-                message: "Категория была удалена"
+                message: "Категория была удален"
             });
-        } catch (e) {}
+        } catch (e) {
+            res.status(500).json({
+                message: "На сервере возникла ошибка. Попробуйте позже.",
+                error: e.message
+            });
+        }
     }
 }
+
 module.exports = new CategoryController();
