e721608a4a0472ac96a49b94fc145888fa4ed4dd
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-17 23:56:06 +0300

    Refactoring

--- src/pages/User/userSettings.jsx
+++ src/pages/User/userSettings.jsx
@@ -1,130 +1,215 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // reactstrap components
-import {
-    Button,
-    FormGroup,
-    Form,
-    Input,
-    Row,
-    Col,
-    CustomInput
-} from "reactstrap";
+import { Button, Form, Row, Col } from "reactstrap";
 
 // core components
 import Page from "../../components/page";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    getAuthErrors,
+    getCurrentUserData,
+    getUsersLoadingStatus,
+    updateUserData
+} from "../../store/users";
+import TextField from "../../components/form/fields/textField";
+import { getUserValidationSchema } from "../../utils/getUserValidationSchema";
+import SelectField from "../../components/form/fields/selectField";
+import { CITY_LIST } from "../../utils/consts";
+import _ from "lodash";
 
-function UserSettings() {
+function UserSettings({ specifiedUser }) {
+    let currentUser = useSelector(getCurrentUserData());
+    const user = specifiedUser || currentUser;
+    console.log("user", user._id);
+    const [data, setData] = useState({ ...prepareData(user) });
+    const [errors, setErrors] = useState({});
+    const dispatch = useDispatch();
+    const isLoaded = !useSelector(getUsersLoadingStatus());
+    const validateSchema = getUserValidationSchema();
+    const userUpdateError = useSelector(getAuthErrors());
     document.documentElement.classList.remove("nav-open");
     React.useEffect(() => {
         document.body.classList.add("settings-page");
-        window.scrollTo(0, 0);
-        document.body.scrollTop = 0;
+        // window.scrollTo(0, 0);
+        // document.body.scrollTop = 0;
         return function cleanup() {
             document.body.classList.remove("settings-page");
         };
     });
-    return (
-        <Page title="Мои данные">
-            <Row>
-                <Col className="ml-auto mr-auto" md="6">
-                    <Form className="settings-form">
-                        <Row>
-                            <Col md="6" sm="6">
-                                <FormGroup>
-                                    <label>First Name</label>
-                                    <Input
-                                        className="border-input"
-                                        placeholder="First Name"
-                                        type="text"
-                                    />
-                                </FormGroup>
-                            </Col>
-                            <Col md="6" sm="6">
-                                <FormGroup>
-                                    <label>Last Email</label>
-                                    <Input
-                                        className="border-input"
-                                        placeholder="Last Name"
-                                        type="text"
-                                    />
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <FormGroup>
-                            <label>Job Title</label>
-                            <Input
-                                className="border-input"
-                                placeholder="Job Title"
-                                type="text"
+    const validate = () => {
+        validateSchema
+            .validate(data)
+            .then(() => setErrors({}))
+            .catch((err) => setErrors({ [err.path]: err.message }));
+        return isValid();
+    };
+    const isValid = () => {
+        return (
+            isLoaded &&
+            Object.keys(data).length > 0 &&
+            Object.keys(errors).length === 0 &&
+            Object.values(data).join("").length > 0
+        );
+    };
+    useEffect(() => {
+        if (Object.keys(data).length > 0) {
+            validate();
+        }
+        // eslint-disable-next-line
+    }, [data]);
+    const handleChange = (target) => {
+        setData((prevState) => {
+            return {
+                ...prevState,
+                [target.name]: target.value
+            };
+        });
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isValid = validate();
+        if (!isValid) return;
+        dispatch(updateUserData(user._id, data));
+    };
+    const body = (
+        <Row>
+            <Col className="ml-auto mr-auto" md="6">
+                <Form className="settings-form" onSubmit={handleSubmit}>
+                    <Row>
+                        <Col md="6">
+                            <TextField
+                                label="Имя"
+                                name="name"
+                                required={true}
+                                onChange={handleChange}
+                                autoFocus
+                                value={data.name}
+                                error={errors.name}
                             />
-                        </FormGroup>
-                        <FormGroup>
-                            <label>Description</label>
-                            <Input
-                                placeholder="This is a textarea limited to 150 characters."
-                                className="textarea-limited"
-                                type="textarea"
-                                maxLength="150"
-                                rows="3"
+                        </Col>
+                        <Col md="6">
+                            <TextField
+                                label="Фамилия"
+                                name="lastName"
+                                required={true}
+                                onChange={handleChange}
+                                value={data.lastName}
+                                error={errors.lastName}
                             />
-                            <h5>
-                                <small>
-                                    <span
-                                        className="pull-right"
-                                        id="textarea-limited-message"
-                                    >
-                                        150 characters left
-                                    </span>
-                                </small>
-                            </h5>
-                        </FormGroup>
-                        <label>Notifications</label>
-                        <ul className="notifications">
-                            <li className="notification-item d-flex justify-content-between align-items-center">
-                                Updates regarding platform changes{" "}
-                                <CustomInput
-                                    type="switch"
-                                    defaultChecked
-                                    id="exampleCustomSwitch-1"
-                                    name="customSwitch-1"
-                                    className="custom-switch-info"
-                                />
-                            </li>
-                            <li className="notification-item d-flex justify-content-between align-items-center">
-                                Updates regarding product changes{" "}
-                                <CustomInput
-                                    type="switch"
-                                    defaultChecked
-                                    id="exampleCustomSwitch-2"
-                                    name="customSwitch-2"
-                                    className="custom-switch-info"
-                                />
-                            </li>
-                            <li className="notification-item d-flex justify-content-between align-items-center">
-                                Weekly newsletter{" "}
-                                <CustomInput
-                                    type="switch"
-                                    defaultChecked
-                                    id="exampleCustomSwitch-3"
-                                    name="customSwitch-3"
-                                    className="custom-switch-info"
-                                />
-                            </li>
-                        </ul>
-                        <div className="text-center">
-                            <Button
-                                className="btn-wd btn-round"
-                                color="info"
-                                type="submit"
-                            >
-                                Save
-                            </Button>
-                        </div>
-                    </Form>
-                </Col>
-            </Row>
-        </Page>
-    );
-}
+                        </Col>
+                    </Row>
 
+                    <Row>
+                        <Col md="6">
+                            <div className="js-form-message">
+                                <TextField
+                                    label="Телефон"
+                                    name="phone"
+                                    required={true}
+                                    onChange={handleChange}
+                                    value={data.phone}
+                                    error={errors.phone}
+                                />
+                            </div>
+                        </Col>
+                        <Col md="6">
+                            <div className="mb-4">
+                                <SelectField
+                                    label="Город"
+                                    defaultOption="Выберите..."
+                                    name="city"
+                                    options={CITY_LIST}
+                                    defaultValue={data.city}
+                                    onChange={handleChange}
+                                    error={errors.city}
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col md="8">
+                            <div className="js-form-message">
+                                <TextField
+                                    label="Адрес доставки"
+                                    name="address"
+                                    required={true}
+                                    onChange={handleChange}
+                                    value={data.address}
+                                    error={errors.address}
+                                />
+                            </div>
+                        </Col>
+                        <Col md="4">
+                            <div className="js-form-message">
+                                <TextField
+                                    label="Почтовый индекс"
+                                    name="postCode"
+                                    required={false}
+                                    onChange={handleChange}
+                                    value={data.postCode}
+                                    error={errors.postCode}
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md="6">
+                            <TextField
+                                label="Email"
+                                name="email"
+                                required={true}
+                                onChange={handleChange}
+                                value={data.email}
+                                error={errors.email}
+                            />
+                        </Col>
+                        <Col md="6">
+                            <TextField
+                                label="Новый пароль"
+                                name="password"
+                                type="password"
+                                required={false}
+                                onChange={handleChange}
+                                value={data.password}
+                                error={errors.password}
+                            />
+                        </Col>
+                    </Row>
+                    {userUpdateError && (
+                        <Row>
+                            <span className="text-danger mt-2 ml-auto mr-5 font-weight-bold">
+                                {userUpdateError}
+                            </span>
+                        </Row>
+                    )}
+                    <Row className="justify-content-center">
+                        <Button
+                            type="submit"
+                            className="btn-round btn-lg  mt-3"
+                            color="danger"
+                            disabled={!isValid()}
+                        >
+                            Сохранить
+                        </Button>
+                    </Row>
+                </Form>
+            </Col>
+        </Row>
+    );
+    return !specifiedUser ? <Page title="Мои данные">{body}</Page> : body;
+}
+function prepareData(user) {
+    const userShaped = _.omit(user, [
+        "updatedAt",
+        "createdAt",
+        "__v",
+        "_id",
+        "role"
+    ]);
+    return {
+        ...userShaped,
+        password: ""
+    };
+}
 export default UserSettings;
