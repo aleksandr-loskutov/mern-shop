b2eaf5eb8857a3e626279fa0edeaef743df4123e
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-17 22:51:38 +0300

    Added admin stats

---
+++ src/components/loremData.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { Card, CardBody } from "reactstrap";
+import Page from "./page";
+
+function LoremData(props) {
+    return (
+        <Page title={"Информация"}>
+            {" "}
+            <Card className="card-refine">
+                <CardBody>
+                    <div>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Animi architecto at beatae delectus dolores
+                        doloribus ducimus enim, esse excepturi, fuga ipsa iusto
+                        labore laborum natus nesciunt officia officiis quod
+                        repudiandae?
+                    </div>
+                    <div>
+                        Beatae ea esse explicabo magnam obcaecati. Animi,
+                        architecto aut dolores earum error, esse impedit iste
+                        maiores minima, numquam odio perspiciatis possimus
+                        quaerat quia repellendus sapiente tempora tempore velit
+                        vero voluptate?
+                    </div>
+                    <div>
+                        Animi, cum cupiditate eveniet exercitationem iure maxime
+                        natus necessitatibus omnis pariatur provident quam quasi
+                        repudiandae suscipit? Cumque enim error est ex expedita
+                        natus non obcaecati, placeat possimus? Iste, optio
+                        repudiandae!
+                    </div>
+                    <div>
+                        Alias aspernatur deleniti dolores eius ipsa possimus
+                        quae? Aspernatur consequuntur dolore ducimus, eaque eos
+                        mollitia voluptates! Aliquam consequatur deleniti
+                        deserunt eius iste iusto nisi quia, sapiente soluta
+                        ullam unde voluptate?
+                    </div>
+                    <div>
+                        Consequatur dolore dolorem dolorum earum, exercitationem
+                        id ipsum magnam possimus qui quibusdam repellendus
+                        similique tempore tenetur voluptas voluptatibus?
+                        Accusamus beatae error esse id praesentium sapiente sit
+                        sunt vero! Doloremque, fuga?
+                    </div>
+                    <div>
+                        Ad animi atque eaque, est exercitationem facere ipsa
+                        mollitia nobis placeat quaerat quibusdam quisquam ut. At
+                        autem distinctio, earum enim, esse facere iure
+                        laboriosam magni maxime molestiae nostrum sit tenetur.
+                    </div>
+                    <div>
+                        Assumenda doloribus id magnam magni minima molestiae
+                        nam, possimus quisquam ratione repudiandae, temporibus
+                        tenetur vero, voluptatibus. Blanditiis dolore natus
+                        unde. Accusantium, amet ducimus hic minima nisi numquam
+                        similique vitae voluptates.
+                    </div>
+                    <div>
+                        At consequatur cumque eaque, quaerat quod rem similique.
+                        Culpa doloribus eum in neque officia perferendis
+                        perspiciatis repellat veniam voluptas voluptatem.
+                        Aliquid eligendi esse labore magnam non pariatur quo
+                        reiciendis, reprehenderit.
+                    </div>
+                    <div>
+                        Aliquam architecto beatae culpa deserunt dolore eaque
+                        excepturi exercitationem expedita fugiat labore magni
+                        nostrum nulla odio porro possimus provident quae quam
+                        quas quibusdam, quisquam quod repellendus, reprehenderit
+                        sed tenetur voluptatem.
+                    </div>
+                    <div>
+                        Ab accusamus aliquam architecto aut beatae cum dicta
+                        dolores earum eos error et facere in iste laborum libero
+                        neque non nulla, officia quam quisquam, sunt, tenetur
+                        unde ut veritatis voluptatum!
+                    </div>
+                </CardBody>
+            </Card>{" "}
+        </Page>
+    );
+}
+
+export default LoremData;
--- src/pages/Admin/admin.jsx
+++ src/pages/Admin/admin.jsx
@@ -1,10 +1,128 @@
 import React from "react";
 import PageAdmin from "../../components/pageAdmin";
+import { useSelector } from "react-redux";
+import { getProducts } from "../../store/products";
+import { getUsersList } from "../../store/users";
+import { getOrders } from "../../store/orders";
+import { getCategories } from "../../store/categories";
+import { Card, CardBody, CardTitle, Col, Row } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Admin = () => {
+    const products = useSelector(getProducts());
+    const users = useSelector(getUsersList());
+    const orders = useSelector(getOrders());
+    const categories = useSelector(getCategories());
+    const sum =
+        orders.length > 0
+            ? orders.reduce(
+                  (acc, o) => acc + (o.payment ? Number(o.total) : 0),
+                  0
+              )
+            : 0;
+    const profit = sum * 0.2;
     return (
         <PageAdmin title="Статистика магазина">
-            <div>AdminStats</div>
+            <Row className="coloured-cards">
+                <Col md="4" sm="6">
+                    <div className="card-big-shadow">
+                        <Link to="/admin/categories/">
+                            <Card
+                                className="card-just-text"
+                                data-color="blue"
+                                data-radius="none"
+                            >
+                                <CardBody>
+                                    <CardTitle tag="h3">Категорий</CardTitle>
+                                    <h2>{categories.length}</h2>
+                                </CardBody>
+                            </Card>
+                        </Link>
+                        {/* end card */}
+                    </div>
+                </Col>
+                <Col md="4" sm="6">
+                    <div className="card-big-shadow">
+                        <Link to="/admin/products/">
+                            <Card
+                                className="card-just-text"
+                                data-color="green"
+                                data-radius="none"
+                            >
+                                <CardBody>
+                                    <CardTitle tag="h3">Товаров</CardTitle>
+                                    <h2>{products.length}</h2>
+                                </CardBody>
+                            </Card>
+                        </Link>
+                        {/* end card */}
+                    </div>
+                </Col>
+                <Col md="4" sm="6">
+                    <div className="card-big-shadow">
+                        <Link to="/admin/orders/">
+                            <Card
+                                className="card-just-text"
+                                data-color="yellow"
+                                data-radius="none"
+                            >
+                                <CardBody>
+                                    <CardTitle tag="h3">Заказов</CardTitle>
+                                    <h2>{orders.length}</h2>
+                                </CardBody>
+                            </Card>
+                        </Link>
+                        {/* end card */}
+                    </div>
+                </Col>
+                <Col md="4" sm="6">
+                    <div className="card-big-shadow">
+                        <Link to="/admin/users/">
+                            <Card
+                                className="card-just-text"
+                                data-color="brown"
+                                data-radius="none"
+                            >
+                                <CardBody>
+                                    <CardTitle tag="h3">Регистраций</CardTitle>
+                                    <h2>{users.length}</h2>
+                                </CardBody>
+                            </Card>
+                        </Link>
+                        {/* end card */}
+                    </div>
+                </Col>
+                <Col md="4" sm="6">
+                    <div className="card-big-shadow">
+                        <Card
+                            className="card-just-text"
+                            data-color="purple"
+                            data-radius="none"
+                        >
+                            <CardBody>
+                                <CardTitle tag="h3">Приход</CardTitle>
+                                <h2>{sum} ₽</h2>
+                            </CardBody>
+                        </Card>
+                        {/* end card */}
+                    </div>
+                </Col>
+                <Col md="4" sm="6">
+                    <div className="card-big-shadow">
+                        <Card
+                            className="card-just-text"
+                            data-color="orange"
+                            data-radius="none"
+                        >
+                            <CardBody>
+                                <CardTitle tag="h3">Прибыль</CardTitle>
+                                <h2>{profit} ₽</h2>
+                            </CardBody>
+                        </Card>
+                        {/* end card */}
+                    </div>
+                </Col>
+            </Row>
         </PageAdmin>
     );
 };
