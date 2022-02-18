14848dac27ad0c2d6ebc34a090a3f75beefd92bf
Aleksandr.L <xvxxxy@yandex.ru>
2022-02-18 23:08:31 +0300

    Added validation

--- src/utils/getUserValidationSchema.js
+++ src/utils/getUserValidationSchema.js
@@ -1,11 +1,28 @@
 import * as yup from "yup";
+import { CITY_LIST } from "./consts";
 
 export function getUserValidationSchema() {
     return yup.object().shape({
-        name: yup.string().required("Укажите имя"),
+        email: yup
+            .string()
+            .required("Email обязателен")
+            .email("Email введен не корректно"),
+        address: yup.string().required("Укажите адрес"),
+        city: yup.string().oneOf(
+            [
+                ...CITY_LIST.reduce((acc, cityObj) => {
+                    acc.push(cityObj.value);
+                    return acc;
+                }, []),
+                null
+            ],
+            "Выберите город"
+        ),
+        phone: yup
+            .string()
+            .required("Укажите телефон")
+            .matches(/^\+?7(\d{10})$/, "Укажите корректный номер через +7"),
         lastName: yup.string().required("Укажите фамилию"),
-        phone: yup.string().required("Укажите телефон"),
-        city: yup.string().required("Укажите город"),
-        address: yup.string().required("Укажите адрес")
+        name: yup.string().required("Укажите имя")
     });
 }
