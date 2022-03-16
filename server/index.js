0ef0413484b1b0d0a7c34e97f98daa8588130e6f
Aleksandr.L <xvxxxy@yandex.ru>
2022-03-16 12:53:50 +0300

    Added cron job to restore mock data

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const app = express();
 const cors = require("cors");
 const router = require("./routes/index");
 const errorHandler = require("./middleware/errorHandler");
+const CronJob = require("cron").CronJob;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
@@ -18,27 +19,33 @@ app.use("/uploads", express.static("uploads"));
 app.use(errorHandler);
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "client")));
-  const indexPath = path.join(__dirname, "client", "index.html");
-  app.get("*", (req, res) => {
-    res.sendFile(indexPath);
-  });
+    app.use("/", express.static(path.join(__dirname, "client")));
+    const indexPath = path.join(__dirname, "client", "index.html");
+    app.get("*", (req, res) => {
+        res.sendFile(indexPath);
+    });
 }
 
 const start = async () => {
-  try {
-    db.mongoose.connection.once("open", () => {
-      initDatabase();
-    });
-    await db.mongoose.connect(DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    app.listen(PORT, () => {
-      console.log("server started on port", PORT);
-    });
-  } catch (e) {
-    console.log("ошибка", e);
-  }
+    try {
+        db.mongoose.connection.once("open", () => {
+            initDatabase();
+        });
+        await db.mongoose.connect(DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        app.listen(PORT, () => {
+            console.log("server started on port", PORT);
+        });
+    } catch (e) {
+        console.log("ошибка", e);
+    }
 };
 start();
+
+// cron -> restore mock data every 30 mins
+const cronRestoreMockData = new CronJob("0 */30 * * * *", () => {
+    initDatabase();
+});
+cronRestoreMockData.start();
