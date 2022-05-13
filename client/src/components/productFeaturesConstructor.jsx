a48e2100abed8257b8e61b33be15640c32ff5f91
Aleksandr.L <loskutov@aleksandrl.ru>
2022-05-13 20:59:36 +0300

    Refactoring & fixed some bugs

--- client/src/components/productFeaturesConstructor.jsx
+++ client/src/components/productFeaturesConstructor.jsx
@@ -3,26 +3,28 @@ import { Col, Row } from "reactstrap";
 import DoubleSelect from "./doubleSelect";
 import { getFeaturesFromProducts } from "../utils/getFeaturesFromProducts";
 
-//todo: refactoring & optimization
-function ProductFeaturesConstructor({ products, featuresData, onChange }) {
-    const [data, setData] = useState(featuresData);
+function ProductFeaturesConstructor({ featuresData, products, onChange }) {
+    const isFeatures = featuresData.length > 0;
     const features = useRef();
-    useEffect(() => {
-        onChange(data);
-        // eslint-disable-next-line
-    }, [data]);
+    const removeLastFeature = useRef(false);
+    features.current = getFeaturesFromProducts(products);
+    const defaultFeatures = [
+        { name: features.current.keys[0].value, value: "" }
+    ];
+    const [data, setData] = useState(
+        isFeatures ? featuresData : defaultFeatures
+    );
 
     useEffect(() => {
-        features.current = getFeaturesFromProducts(products);
-        features.current?.keys.length > 0 &&
-            setData([
-                {
-                    name: features.current.keys[0].value,
-                    value: ""
-                }
-            ]);
+        if (!isFeatures) setData(defaultFeatures);
         // eslint-disable-next-line
-    }, [products]);
+    }, [featuresData]);
+
+    useEffect(() => {
+        onChange(data);
+        setNextFeature();
+        // eslint-disable-next-line
+    }, [data]);
 
     const handleSelectFeature = (value) => {
         setData((prevState) => {
@@ -38,7 +40,30 @@ function ProductFeaturesConstructor({ products, featuresData, onChange }) {
         });
     };
 
-    useEffect(() => {
+    const getNextFeatureIndex = (index) => {
+        const featuresInData = data.reduce((acc, feature) => {
+            acc.push(feature.name);
+            return acc;
+        }, []);
+        if (!featuresInData.includes(features.current.keys[index].label)) {
+            return index;
+        }
+        let nextFeatureIndex = index;
+        features.current.keys.every((f, index) => {
+            if (!featuresInData.includes(f.label)) {
+                nextFeatureIndex = index;
+                return false;
+            }
+            return true;
+        });
+        return nextFeatureIndex;
+    };
+
+    function setNextFeature(reset = false) {
+        if (removeLastFeature.current) {
+            removeLastFeature.current = false;
+            return;
+        }
         if (data?.length > 0) {
             const count = data.reduce(
                 (acc, feature) =>
@@ -46,24 +71,64 @@ function ProductFeaturesConstructor({ products, featuresData, onChange }) {
                 0
             );
             if (
-                count === data.length &&
-                data.length < features?.current?.keys.length
+                (count === data.length &&
+                    data.length < features?.current?.keys.length) ||
+                reset
             ) {
                 const nextFeature = {
-                    name: features.current.keys[data.length].value,
+                    name: features.current.keys[
+                        reset ? 0 : getNextFeatureIndex(data.length)
+                    ].value,
                     value: ""
                 };
-                setData((prevState) => [...prevState, nextFeature]);
+                setData((prevState) => {
+                    return reset ? [] : [...prevState, nextFeature];
+                });
             }
         }
-    }, [data]);
+    }
+
     const reset = () => {
-        setData([
-            {
-                name: features.current.keys[0].value,
-                value: ""
-            }
-        ]);
+        setNextFeature(true);
+    };
+
+    const getDefaultValueForDoubleSelect = (featureName) => {
+        const featureObj =
+            features.current.keys[
+                features.current.keys.findIndex((f) => f.label === featureName)
+            ];
+
+        return data.length > 0
+            ? {
+                  ...featureObj,
+                  secondDefault:
+                      data[data.findIndex((f) => f.name === featureName)]
+                          ?.value ?? ""
+              }
+            : featureObj;
+    };
+
+    const getFeatureValuesOptions = (name, index) => {
+        return features.current.all[name].map((f) => {
+            return {
+                label: f,
+                value: f,
+                key: name,
+                input: "value",
+                index: index
+            };
+        });
+    };
+
+    const getFeatureOptions = (index) => {
+        return features.current.keys
+            .map((f) => {
+                return {
+                    ...f,
+                    index
+                };
+            })
+            .filter((f) => !data.some((feat) => feat.name === f.value));
     };
 
     const getConstructorInputs = () => {
@@ -73,51 +138,58 @@ function ProductFeaturesConstructor({ products, featuresData, onChange }) {
                       <DoubleSelect
                           key={index}
                           index={index}
-                          featureOptions={features.current.keys
-                              .map((f) => {
-                                  return {
-                                      ...f,
-                                      index: index
-                                  };
-                              })
-                              .filter(
-                                  (f) =>
-                                      !data.some(
-                                          (feat) => feat.name === f.value
-                                      )
-                              )}
-                          featureValuesOptions={features.current.all[
+                          featureOptions={getFeatureOptions(index)}
+                          featureValuesOptions={getFeatureValuesOptions(
+                              feature.name,
+                              index
+                          )}
+                          defaultValue={getDefaultValueForDoubleSelect(
                               feature.name
-                          ].map((f) => {
-                              return {
-                                  label: f,
-                                  value: f,
-                                  key: feature.name,
-                                  input: "value",
-                                  index: index
-                              };
-                          })}
-                          defaultValue={features.current.keys[index]}
+                          )}
                           onSelect={handleSelectFeature}
                       />
                   );
               })
             : null;
     };
-
+    const deleteLastFeature = () => {
+        removeLastFeature.current = true;
+        setData((prevState) => {
+            const state = [...prevState];
+            state.pop();
+            return state;
+        });
+    };
+    const getDeleteLastFeatureButton = () => {
+        return (
+            data.length > 1 && (
+                <Row className="">
+                    <Col>
+                        <a
+                            className="text-info pull-right"
+                            href="#link"
+                            onClick={deleteLastFeature}
+                        >
+                            <span className="text-info">[ удалить ]</span>
+                        </a>
+                    </Col>
+                </Row>
+            )
+        );
+    };
     return (
         <>
             <Row>
-                <Col className="text-center">
-                    <span className="mt-0 mb-2 mr-2">
-                        конструктор параметров
-                    </span>
-                    <a className="text-info" href="#link" onClick={reset}>
-                        ( сбросить )
+                <Col className="text-center mt-2 mb-2 ">
+                    <span className="text-dark">конструктор параметров</span>
+                    <a className="text-info " href="#link" onClick={reset}>
+                        {" "}
+                        [ сбросить ]
                     </a>
                 </Col>
             </Row>
             {getConstructorInputs()}
+            {getDeleteLastFeatureButton()}
         </>
     );
 }
